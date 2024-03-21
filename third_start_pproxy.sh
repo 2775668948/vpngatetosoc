5fccd3df4bbcd98493c2fc27d8f85811bc8fcd11
kxuan <kxuan1320@gmail.com>
2024-03-21 17:17:02 +0800

    ok

--- third_start_pproxy.sh
+++ third_start_pproxy.sh
@@ -1,1 +1,1 @@
-docker run -d --network="host" --name=pproxy leenwu/pproxy pproxy -l ss://chacha20:abc@:8080 -r socks5://localhost:1081
+docker run -d --network="host" --name=pproxy kxuan998/pproxy -l ss://chacha20:abc@:8080 -r socks5://localhost:1081
